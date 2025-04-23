59ebe23781baf641475ae83c8f44c752252c15c0
Hurrtt <Ethangarcia3743@gmail.com>
2025-04-22 20:25:56 -0600

    Terminamos menu

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from CRUD.buscar import buscar_cliente
 from CRUD.eliminar import eliminar_cliente
+from CRUD.agregar import agregar_cliente
+from CRUD.editar import edit_client
 
 def menu():
     opc=""
-    while opc != "6":
+    while opc != "5":
         print("\n Bienvenido al centro de datos")
-        print("\n Seleccione una opcion: \n1-Buscar un cliente \n2-eliminar un cliente \n3-Buscar un libro \n4-Actualizar un libro \n5-Eliminar libro \n6-Salir")
+        print("\n Seleccione una opcion: \n1-Buscar un cliente \n2-eliminar un cliente \n3-Agregar cliente \n4-Actualizar informacion de un cliente \n5-Salir")
         opc=input()
         if opc == "1":
             buscar_cliente()
         elif opc == "2":
             eliminar_cliente()
         elif opc == "3":
-            print("Agrega tu funcion")
+            agregar_cliente()
         elif opc == "4":
-            print("Agrega tu funcion")
+            edit_client()
         elif opc == "5":
-            print("Agrega tu funcion")
-        elif opc == "6":
             print("Hasta luego")
 menu()
